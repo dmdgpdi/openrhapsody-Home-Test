6f41abbf8fc5d3be9806dbd63c00fd503fb5126c
dmdgpdi <dmdgpdi@naver.com>
2025-02-28 06:33:46 +0900

    chore: 의존성 설치 및 세팅

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  roots: ['<rootDir>'],
+};
+
+export default config;
